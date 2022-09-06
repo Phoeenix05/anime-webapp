e1bcec076d9540733aeced8b33e5cd49141dc7b5
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2022-09-06 13:22:24 +0300

    Fixed home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return <>
     <h1>Home</h1>
     <div class="content">
-      <AnimeList title="Trending" url="https://kitsu.io/api/edge/trending/anime" />
+      <AnimeList title="Trending" href="https://kitsu.io/api/edge/trending/anime" limit={20} />
     </div>
   </>
 }
